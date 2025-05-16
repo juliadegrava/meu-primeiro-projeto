9eb94b641ccc2a4b0f0a449b8b7e5abe872459a0
Júlia Degrava <juhcdgomes@gmail.com>
2025-05-16 11:36:32 -0300

    Cria o arquivo modelo02.js e multiplica dois números

---
+++ modelo02.js
@@ -0,0 +1,8 @@
+// multiplicação de dois números
+let numero1 = 89;
+let numero2 = 3;
+let multiplicacao;
+
+soma = numero1 * numero2;
+
+console.log(`a multiplicacao de ${numero1} e ${numero2} é ${multiplicacao}.`);
